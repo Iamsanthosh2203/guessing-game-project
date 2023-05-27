e76d02591d55d20ad7a3979cc2494bb57f351b51
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-05-27 21:45:11 +0530

    new

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let randNum = Math.trunc(Math.random() * 20 + 1);
+
+const message = document.querySelector('.message');
+
+const checkBtn = document.querySelector('.btn.check');
+
+const score = document.querySelector('.score');
+
+const number = document.querySelector('.number');
+
+const input = document.querySelector('input');
+
+const body = document.querySelector('body');
+
+const again = document.querySelector('.again');
+
+let highScore = document.querySelector('.highscore');
+
+score.innerHTML = Number(20);
+highScore.innerHTML = Number(0);
+
+checkBtn.addEventListener('click', () => {
+  number.innerHTML = input.value;
+
+  if (!input.value) {
+    message.textContent = '❌ No Number';
+  } else if (input.value < 0 || input.value > 20) {
+    message.textContent = '❕ Enter number between 1 to 20';
+  } else {
+    message.textContent = 'Start Guessing...';
+  }
+
+  if (input.value == randNum) {
+    message.textContent = '🏆 YOU WON !!!';
+    highScore.innerHTML = Number(highScore.innerHTML) + Number(score.innerHTML);
+    body.style.backgroundColor = 'green';
+  } else if (input.value < randNum) {
+    message.textContent = '🤦‍♀️ Lesser Number';
+    score.innerHTML = Number(score.innerHTML - 1);
+    body.style.backgroundColor = 'red';
+  } else if (input.value > randNum) {
+    message.textContent = '🤦‍♀️ Higher Number';
+    score.innerHTML = Number(score.innerHTML - 1);
+    body.style.backgroundColor = 'red';
+  }
+});
+
+again.addEventListener('click', () => {
+  randNum = Math.trunc(Math.random() * 20 + 1);
+  body.style.backgroundColor = 'black';
+  number.innerHTML = '?';
+  return (score.innerHTML = 20);
+});
