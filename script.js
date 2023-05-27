cfd5db441cfdd41c6c40fe7b2cfe4ba07e647758
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-05-27 22:02:20 +0530

    updated-js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// ALL DOCUMENT MANUPULATION
 let randNum = Math.trunc(Math.random() * 20 + 1);
 
 const message = document.querySelector('.message');
@@ -18,24 +19,25 @@ const again = document.querySelector('.again');
 
 let highScore = document.querySelector('.highscore');
 
+// SETTING NUMBER
 score.innerHTML = Number(20);
 highScore.innerHTML = Number(0);
 
+// FUNCTIONS
 checkBtn.addEventListener('click', () => {
+  // UPDATING INPUT VALUE
   number.innerHTML = input.value;
 
-  if (!input.value) {
-    message.textContent = '❌ No Number';
-  } else if (input.value < 0 || input.value > 20) {
-    message.textContent = '❕ Enter number between 1 to 20';
-  } else {
-    message.textContent = 'Start Guessing...';
-  }
-
+  // GAME LOGIC
   if (input.value == randNum) {
     message.textContent = '🏆 YOU WON !!!';
     highScore.innerHTML = Number(highScore.innerHTML) + Number(score.innerHTML);
     body.style.backgroundColor = 'green';
+  } else if (input.value == '') {
+    message.textContent = '❌ No Number';
+    number.innerHTML = '?';
+  } else if (input.value < 0 || input.value > 20) {
+    message.textContent = '❕ Enter number between 1 to 20';
   } else if (input.value < randNum) {
     message.textContent = '🤦‍♀️ Lesser Number';
     score.innerHTML = Number(score.innerHTML - 1);
@@ -47,6 +49,7 @@ checkBtn.addEventListener('click', () => {
   }
 });
 
+// RESET FUNCTION
 again.addEventListener('click', () => {
   randNum = Math.trunc(Math.random() * 20 + 1);
   body.style.backgroundColor = 'black';
